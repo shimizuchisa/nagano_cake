75bae67658c0a553cb5d73823fd675cd02d3a258
EC2 Default User <ec2-user@ip-172-31-2-47.ap-northeast-1.compute.internal>
2022-10-06 20:50:26 +0000

    add genre

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+    resources :genres, only:[:index, :create, :edit, :update]
   end
-# 顧客用
+
+
+# devise顧客用
 # URL /customers/sign_in ...
 devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
+# devise管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
