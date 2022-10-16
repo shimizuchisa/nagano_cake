efd1deb21ee459608e68b998f7cfb8534e050122
EC2 Default User <ec2-user@ip-172-31-2-47.ap-northeast-1.compute.internal>
2022-10-16 19:18:56 +0000

    add cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,27 +1,28 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = Cart_item.all
+    @cart_items = CartItem.all
   end
 
   def create
-    @cart_item = Cart_item.new(cart_item_params)
-    @cart_item.save
+    # @item = Item.find(params[:id])
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.save
     redirect_to cart_items_path
   end
 
   def update
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
 
   end
 
   def destroy
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
   end
 
   def destroy_all
-    @cart_items = Cart_item.all
+    @cart_items = CartItem.all
     @cart_items.destroy
   end
 
